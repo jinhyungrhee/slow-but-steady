6205aeebbdf8a886addfc3e00c4724be2babcb38
jinhyung rhee <rheejinhyung@gmail.com>
2021-02-25 22:03:53 +0900

    update lambda-ex

--- Python/lambda.py
+++ Python/lambda.py
@@ -23,33 +23,39 @@ a.sort(key=int)
 print(a) # ['7', '16', '25'] -> int 
 '''
 
+
 # <lambda + map>
 # 제곱
 b = list(map(lambda x: x*x, [4, 2, 7]))
 print(b)
 
+
 # 절대값 
 a = [-5, 3, -2]
 b = list(map(lambda x: -x if x < 0 else x, a))
 print(b)
-'''
+
+
 # 한 줄에 여러 개의 정수 값 입력받기(int함수 사용)
 a = list(map(int, input().split()))
 b = [int(x) for x in input().split()]
 print(a)
 print(b)
-'''
+
+
 # 두 리스트를 더하기
 a = [1, 2, 3]
 b = [4 ,5, 6]
 add = lambda x, y: x+y
 print(list(map(add, a, b))) # [5, 7, 9]
 
+
 # <lambda + filter>
 # 짝수만 모으기
 a = [1, 5, 3, 4, 6, 7, 10, 12]
 b = list(filter(lambda x: x%2 == 0, a))
 print(b)
+
 
 # <lambda + reduce>
 # 리스트의 각 원소 모두 더하기
