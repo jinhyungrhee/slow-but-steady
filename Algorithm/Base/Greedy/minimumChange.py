1309a6860c054d655cdb4879c211181e9674991a
jinhyung rhee <rheejinhyung@gmail.com>
2022-01-30 15:34:02 +0900

    그리디 : 최소 동전 개수

---
+++ Algorithm/Base/Greedy/minimumChange.py
@@ -0,0 +1,13 @@
+# 거스름돈 문제
+
+n = 1260
+count = 0
+
+# 큰 단위의 화폐부터 차례대로 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+  count += n // coin
+  n %= coin # n = n % coin
+
+print(count)
