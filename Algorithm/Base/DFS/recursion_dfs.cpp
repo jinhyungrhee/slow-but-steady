e9d944e466bf7929cb9bdc36fa271e881a1d7e16
jinhyung rhee <rheejinhyung@gmail.com>
2022-02-08 00:28:50 +0900

    DFS : 재귀호출(cpp)

---
+++ Algorithm/Base/DFS/recursion_dfs.cpp
@@ -0,0 +1,63 @@
+// DFS 예제 - 재귀함수
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 배열은 전역변수로 선언
+bool visited[9];
+vector<int> graph[9]; // 인접 리스트로 구성
+
+// DFS 함수 정의
+void dfs(int x) {
+	// 현재 노드를 방문 처리
+	visited[x] = true;
+	cout << x << " ";
+
+	// 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+	for (int i = 0; i < graph[x].size(); i++) {
+		int y = graph[x][i];
+		if (!visited[y]) dfs(y);
+	}
+
+}
+
+int main() {
+	// 노드 1에 연결된 노드 정보 저장
+	graph[1].push_back(2);
+	graph[1].push_back(3);
+	graph[1].push_back(8);
+
+	// 노드 2에 연결된 노드 정보 저장
+	graph[2].push_back(1);
+	graph[2].push_back(7);
+
+	// 노드 3에 연결된 노드 정보 저장
+	graph[3].push_back(1);
+	graph[3].push_back(4);
+	graph[3].push_back(5);
+
+	// 노드 4에 연결된 노드 정보 저장
+	graph[4].push_back(3);
+	graph[4].push_back(5);
+
+	// 노드 5에 연결된 노드 정보 저장
+	graph[5].push_back(3);
+	graph[5].push_back(4);
+
+	// 노드 6에 연결된 노드 정보 저장
+	graph[6].push_back(7);
+
+	// 노드 7에 연결된 노드 정보 저장
+	graph[7].push_back(2);
+	graph[7].push_back(6);
+	graph[7].push_back(8);
+
+	// 노드 8에 연결된 노드 정보 저장
+	graph[8].push_back(1);
+	graph[8].push_back(7);
+
+	dfs(1);
+
+	return 0;
+}
