40357a1e4f13d2de641cb82cf0172462ac590513
jinhyung rhee <rheejinhyung@gmail.com>
2022-02-08 00:28:11 +0900

    그래프 : 인접 행렬

---
+++ Algorithm/Base/BFS/adjacency_matrix.cpp
@@ -0,0 +1,23 @@
+// 인접 행렬
+
+#include <iostream>
+#define INF 999999999 // 무한의 비용 선언
+using namespace std;
+
+// 2차원 리스트를 이용해 인접 행렬 표현
+int graph[3][3] = {
+	{0, 7, 5},
+	{7, 9, INF},
+	{5, INF, 0}
+};
+
+int main() {
+	// 그래프 출력
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << graph[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
