94ffcbca12d3e9b99f093167d541ec7b9b5ca2f1
jinhyung rhee <rheejinhyung@gmail.com>
2022-04-26 11:51:25 +0900

    Collections : Set Interface 구현 클래스

---
+++ Algorithm/Base/DataStructure/set.java
@@ -0,0 +1,18 @@
+/*
+클래스별 생성 방법
+
+T는 객체 타입 : Integer, String, Double, Long 같은 Wrapper Class부터 사용자 정의 객체까지 가능!
+단, primitive type은 불가능!
+*/
+
+HashSet<T> hashset = new HashSet<>();
+LinkedHashSet<T> linkedhashset = new LinkedHashSet<>();
+TreeSet<T> treeset = new TreeSet<>();
+
+SortedSet<T> treeset = new TreeSet<>();
+
+Set<T> hashset = new HashSet<>();
+Set<T> linkedhashset = new LinkedHashSet<>();
+Set<T> treeset = new TreeSet<>();
+
+// [자료 출처] : https://st-lab.tistory.com/142
