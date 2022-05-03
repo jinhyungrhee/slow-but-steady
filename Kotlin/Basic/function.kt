6f6720044d90decd3a589b8198b0375bbe936c94
jinhyung rhee <rheejinhyung@gmail.com>
2022-05-03 15:00:09 +0900

    코틀린 초급 : 함수

---
+++ Kotlin/Basic/function.kt
@@ -0,0 +1,50 @@
+// 함수(function)
+
+fun main() {
+
+  val sumValue = sum(20, 40)
+  println(sumValue)
+  
+  babMaking(20);
+
+}
+
+fun babMaking(time : Int) {
+  
+  println("$time 분 후에 밥이 완료됩니다!")
+
+}
+
+fun sum(a : Int, b : Int) : Int {
+  
+//     println(a + b)
+  val result = a + b
+  return result
+  
+}
+
+fun abc2() {
+val a = 20
+  val b = 40
+  val c = a + b
+  println(c)
+}
+
+fun abc() {
+  
+  val a = 10
+  val b = 20
+  val c = a + b
+  println(c)
+  
+}
+
+
+fun testFun() {
+  
+  println("a")
+  println("b")
+  println("c")
+  println("d")
+  
+}
