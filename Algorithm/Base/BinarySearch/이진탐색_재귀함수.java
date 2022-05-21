57ac5fae00a08f7010dd2998de81db429dde2823
jinhyung rhee <rheejinhyung@gmail.com>
2022-05-21 12:02:10 +0900

    이진 탐색 : 재귀를 사용한 이진탐색(java)

---
+++ Algorithm/Base/BinarySearch/이진탐색_재귀함수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main{
+
+  public static int binarySearch(int[] array, int target, int start, int end) {
+    if (start > end) return -1;
+    int mid = (start + end) / 2;
+    // 찾은 경우 중간점 인덱스 반환
+    if (array[mid] == target) return mid;
+    // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    else if (array[mid] > target) return binarySearch(array, target, start, mid - 1);
+    // 중간점의 값보다 찾고자 하느 값이 큰 경우 오른쪽 확인
+    else return binarySearch(array, target, mid + 1, end);
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int target = sc.nextInt();
+
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++) {
+      array[i] = sc.nextInt();
+    }
+
+    // 이진 탐색 수행
+    int result = binarySearch(array, target, 0, n - 1);
+    // 출력
+    if (result == -1) System.out.println("원소가 존재하지 않습니다.");
+    else System.out.println(result + 1);
+
+    }
+}
